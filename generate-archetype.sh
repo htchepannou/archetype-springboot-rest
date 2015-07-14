8e0e192f53638b024f10e9bf59a498f2ee63fb9c
htchepannou <htchepannou@expedia.com>
2015-07-14 07:41:20 -0400

    improve template

--- generate-archetype.sh
+++ generate-archetype.sh
@@ -4,7 +4,6 @@ mvn clean install archetype:create-from-project
 
 cd target/generated-sources/archetype
 
-mvn clean
 rm src/main/resources/archetype-resources/generate-archetype.sh
 rm src/main/resources/archetype-resources/*.iml
 rm src/main/resources/archetype-resources/*.md
@@ -13,7 +12,6 @@ rm -Rf src/main/resources/archetype-resources/log
 
 cp ../../../.gitignore src/main/resources/archetype-resources
 cp ../../../README-archetype.md src/main/resources/archetype-resources/README.md
-
 mvn install
 
 cd ../../..
